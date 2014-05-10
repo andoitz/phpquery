dbd381ca9d2d4f3915c0241037598c990f8d65ce
andoitz <andoitzjob@gmail.com>
2014-05-10 19:12:59 +0200

    PHPQuery 0.1
    PHPQuery Terminal is a PHP terminal that works with Ajax and PHP
    Classes. Really easy to use. Write a command and the class with the same
    name will be executed. Also, you can send it a few commands that will be
    used as parameters.

---
+++ class/kernel.php
@@ -0,0 +1,21 @@
+<?php
+/*
+	Kernel load the essential classes
+*/
+class Kernel{
+	public function __construct(){
+		include("commands.php");
+		include("user.php");
+		$commands = new Commands();
+		if($commands->commandMode){
+			include("command.php");
+			$commands->loadCommand();
+		}
+		else{
+			include("template.php");
+			$template = new Template();
+			$template->init('terminal');
+		}
+	}
+}
+?>
---
+++ commands/helloworld.php
@@ -0,0 +1,12 @@
+<?php
+class helloworld extends Command{
+	public function __construct(){
+		parent::__construct();
+	}
+	public function init($params){
+		$result = "Hello World!";
+		if(isset($params[1])) $result.= " ".$params[1];
+		$this->data = $result;
+	}
+}
+?>
